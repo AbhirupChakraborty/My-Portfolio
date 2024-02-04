e2d7185d38a4b7504e6b350361248a333b57cc0f
AbhirupChakraborty <abhirup.chakraborty1998@gmail.com>
2024-02-04 18:40:24 +0530

    add MobileNavbar

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hero.css'
+const Hero = () => {
+  return (
+    <section className="hero-container">
+        <div className="hero-content">
+            <h2>Building Digital Experiences That Inspire</h2>
+            <p>
+                Passionate Frontend Developer | Transforming Ideas into Seamless 
+                and Visually Stunning Web Solutions
+            </p>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import MobileNav from './MobileNav/MobileNav';
 const Navbar = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+    const toggleMenu=()=>{
+        setOpenMenu(!openMenu);
+    };
   return (
     <>
+        <MobileNav isOpen={openMenu} toggleMenu={toggleMenu}/>
         <nav className="nav-wrapper">
             <div className="nav-content">
                 <img className="logo" src="./assets/images/logo.png" alt=""/>
@@ -24,12 +30,13 @@ const Navbar = () => {
                         Hire Me
                     </button>
                 </ul>
-                <button className="menu-btn" onClick={()=>{}}>
+                <button className="menu-btn" onClick={toggleMenu}>
                     <div className="menu-icon">
                         <div></div>
                         <div></div>
                         <div></div>
                     </div>
+                    {openMenu?"":""}
                 </button>
             </div>
         </nav>
